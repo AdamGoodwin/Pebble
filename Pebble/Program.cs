37e49fc521c1cf84c8ec455de98e4c452f049418
AdamGoodwin <Suji.kun@gmail.com>
2023-07-14 21:10:53 -0600

    Partially through Board Representation
    Board Representation is going to be 0x88.  Need to be able to print the Board representation from a given board representation

---
+++ Pebble/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace Pebble
+{
+    public enum Pieces
+    {
+        Pawn = 1,
+        Knight = 2,
+        King = 3,
+        Bishop = 5,
+        Rook = 6,
+        Queen = 7,
+        White = 8,
+        Black = 16
+    }
+
+    public class Board
+    {
+        int[] board;
+        int sideToMove;
+        int enPassantSquare;
+        int halfMoves;
+        int moves;
+
+        public Board()
+        {
+            board = new int[128];
+            sideToMove = 1; // White will be 1.  Black will be -1.
+            enPassantSquare = -1;
+            halfMoves = 0;
+            moves = 0;
+        }
+
+        public void setBoard(string position)
+        {
+            int squareIndex = 112;
+
+            for (int stringPosition = 0; stringPosition < position.Count(); stringPosition++)
+            {
+                switch (position[stringPosition])
+                {
+                    case 'p':
+                        board[squareIndex] = (int)Pieces.Pawn + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'n':
+                        board[squareIndex] = (int)Pieces.Knight + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'b':
+                        board[squareIndex] = (int)Pieces.Bishop + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'r':
+                        board[squareIndex] = (int)Pieces.Rook + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'q':
+                        board[squareIndex] = (int)Pieces.Queen + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'k':
+                        board[squareIndex] = (int)Pieces.King + (int)Pieces.Black;
+                        squareIndex++;
+                        break;
+                    case 'P':
+                        board[squareIndex] = (int)Pieces.Pawn + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case 'N':
+                        board[squareIndex] = (int)Pieces.Knight + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case 'B':
+                        board[squareIndex] = (int)Pieces.Bishop + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case 'R':
+                        board[squareIndex] = (int)Pieces.Rook + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case 'Q':
+                        board[squareIndex] = (int)Pieces.Queen + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case 'K':
+                        board[squareIndex] = (int)Pieces.King + (int)Pieces.White;
+                        squareIndex++;
+                        break;
+                    case '/':
+                        squareIndex -= 24;
+                        break;
+                    default: //Should be a number
+                        int emptySquares = position[stringPosition] - '0';
+                        squareIndex += emptySquares;
+                        break;
+                }
+
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+
+            Console.WriteLine("What position do you want to set up to play?");
+
+            Board newGame = new Board();
+            newGame.setBoard(startingPosition);
+        }
+    }
+}
